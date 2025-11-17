e9bd9a446309630b00934ca2f787205b32f244ad
yub0wang1 <yubohenry.wang@mail.utoronto.ca>
2025-11-17 11:34:09 -0500

    Update EndInteractor.java

--- src/main/java/use_case/end/EndInteractor.java
+++ src/main/java/use_case/end/EndInteractor.java
@@ -1,9 +1,46 @@
 package use_case.end;
 
+import data_access.Language;
+import entity.AbstractWord;
 import interface_adapter.game.GameState;
 
 public class EndInteractor implements EndInputBoundary {
-    public EndInteractor(EndDataAccessInterface endDataAccess, EndOutputBoundary endOutputBoundary) {}
 
-    public void execute(GameState endInputData) {}
+    private final EndDataAccessInterface endDataAccess;
+    private final EndOutputBoundary presenter;
+
+    public EndInteractor(EndDataAccessInterface endDataAccess, EndOutputBoundary presenter) {
+        this.endDataAccess = endDataAccess;
+        this.presenter = presenter;
+    }
+
+    @Override
+    public void execute(GameState gameState) {
+        String wordToGuess = gameState.getWordToGuess().toString();
+        int maxGuesses = gameState.getMaxGuesses();
+        int guessesUsed = gameState.getCurrentGuess();
+        Language language = gameState.getLanguage();
+        boolean won = isWon(gameState);
+
+        EndGameRecord record = new EndGameRecord(
+                wordToGuess,
+                won,
+                guessesUsed,
+                maxGuesses
+        );
+        endDataAccess.saveEndGame(record);
+
+        EndOutputData outputData = new EndOutputData(wordToGuess, won, guessesUsed, maxGuesses);
+    }
+    private boolean playerWon(GameState state) {
+        for (AbstractWord word : state.getWords()) {
+            if (word != null && word.isCorrect()) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
+
+
+
