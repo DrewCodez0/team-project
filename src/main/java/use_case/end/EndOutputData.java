7a45f2c5d179f05d7e57037a52e6bf8ae93b0e94
yub0wang1 <yubohenry.wang@mail.utoronto.ca>
2025-11-17 11:36:25 -0500

    Created EndOutputData.java

---
+++ src/main/java/use_case/end/EndOutputData.java
@@ -0,0 +1,29 @@
+package use_case.end;
+
+public class EndOutputData {
+    private final String wordToGuess;
+    private final boolean won;
+    private final int guessesUsed;
+    private final int maxGuesses;
+
+    public EndOutputData(String wordToGuess, boolean won, int guessesUsed, int maxGuesses) {
+        this.wordToGuess = wordToGuess;
+        this.won = won;
+        this.guessesUsed = guessesUsed;
+        this.maxGuesses = maxGuesses;
+
+    }
+
+    public String getWordToGuess() {
+        return wordToGuess;
+    }
+    public boolean isWon() {
+        return won;
+    }
+    public int getGuessesUsed() {
+        return guessesUsed;
+    }
+    public int getMaxGuesses() {
+        return maxGuesses;
+    }
+}
