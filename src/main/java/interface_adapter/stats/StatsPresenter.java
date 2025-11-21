9a1409e2f7b9555e97a1cf4f9a8a3bedd6a5c1ab
Aathithya Ananth <110254100+XDAathi@users.noreply.github.com>
2025-11-21 14:52:52 -0500

    Working Stats Page following UML diagram, need to complete Back button to StartPage, Data implementation from user input, the layout pretty and Dark Theme.

--- src/main/java/interface_adapter/stats/StatsPresenter.java
+++ src/main/java/interface_adapter/stats/StatsPresenter.java
@@ -3,24 +3,52 @@ package interface_adapter.stats;
 import interface_adapter.ViewManagerModel;
 import interface_adapter.start.StartViewModel;
 import use_case.stats.StatsOutputBoundary;
+import use_case.stats.StatsOutputData;
 
 public class StatsPresenter implements StatsOutputBoundary {
+    private final ViewManagerModel viewManagerModel;
+    private final StatsViewModel statsViewModel;
+    private final StartViewModel startViewModel;
+    private StatsOutputData outputData;
 
     public StatsPresenter(ViewManagerModel viewManagerModel,
                           StatsViewModel statsViewModel,
                           StartViewModel startViewModel) {
+        this.viewManagerModel = viewManagerModel;
+        this.statsViewModel = statsViewModel;
+        this.startViewModel = startViewModel;
+    }
 
+    public void setOutputData(StatsOutputData outputData) {
+        this.outputData = outputData;
     }
 
     @Override
-    public void prepareSuccessView() {}
+    public void prepareSuccessView() {
+        if (outputData != null && outputData.hasStats()) {
+            statsViewModel.setState(outputData.getStatsState());
+            statsViewModel.firePropertyChange();
+        }
+    }
 
     @Override
-    public void prepareFailView() {}
+    public void prepareFailView() {
+        prepareDefaultView();
+    }
 
     @Override
-    public void prepareDefaultView() {}
+    public void prepareDefaultView() {
+        if (outputData != null) {
+            statsViewModel.setState(outputData.getStatsState());
+        } else {
+            statsViewModel.setState(new StatsState());
+        }
+        statsViewModel.firePropertyChange();
+    }
 
     @Override
-    public void prepareStartView() {}
+    public void prepareStartView() {
+        viewManagerModel.setState(startViewModel.getViewName());
+        viewManagerModel.firePropertyChange();
+    }
 }
--- src/main/java/interface_adapter/stats/StatsState.java
+++ src/main/java/interface_adapter/stats/StatsState.java
@@ -1,4 +1,62 @@
 package interface_adapter.stats;
 
+
+import entity.Theme;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class StatsState {
+    private int totalGames;
+    private int gamesWon;
+    private int currentWinStreak;
+    private int longestWinStreak;
+    private Theme theme;
+
+    public StatsState(){
+        this.totalGames = 0;
+        this.gamesWon = 0;
+        this.currentWinStreak = 0;
+        this.longestWinStreak = 0;
+    }
+
+    public int getTotalGames() {
+        return totalGames;
+    }
+
+    public void setTotalGames(int totalGames) {
+        this.totalGames = totalGames;
+    }
+
+    public int getGamesWon() {
+        return gamesWon;
+    }
+
+    public void setGamesWon(int gamesWon) {
+        this.gamesWon = gamesWon;
+    }
+
+    public int getCurrentWinStreak() {
+        return currentWinStreak;
+    }
+
+    public void setCurrentWinStreak(int currentWinStreak) {
+        this.currentWinStreak = currentWinStreak;
+    }
+
+    public int getLongestWinStreak() {
+        return longestWinStreak;
+    }
+
+    public void setLongestWinStreak(int longestWinStreak) {
+        this.longestWinStreak = longestWinStreak;
+    }
+
+    public Theme getTheme() {
+        return theme;
+    }
+
+    public void setTheme(Theme theme) {
+        this.theme = theme;
+    }
 }
