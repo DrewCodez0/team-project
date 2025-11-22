99276f32a8420f415e3ae81d0061a5c78568e6ab
yub0wang1 <yubohenry.wang@mail.utoronto.ca>
2025-11-22 15:48:19 -0500

    Updated EndOutputData.java
    Updated EndOutputData.java, added a win message that displays different texts for guessesUsed, and a lost message that shows the correct word

--- src/main/java/use_case/end/EndOutputData.java
+++ src/main/java/use_case/end/EndOutputData.java
@@ -1,21 +1,36 @@
 package use_case.end;
 
 public class EndOutputData {
-    private final String wordToGuess;
+    private final String word;
     private final boolean won;
     private final int guessesUsed;
     private final int maxGuesses;
+    private final String message;
 
-    public EndOutputData(String wordToGuess, boolean won, int guessesUsed, int maxGuesses) {
-        this.wordToGuess = wordToGuess;
+    public EndOutputData(String word, boolean won, int guessesUsed, int maxGuesses) {
+        this.word = word;
         this.won = won;
         this.guessesUsed = guessesUsed;
         this.maxGuesses = maxGuesses;
 
+        if (won) {
+            this.message = generateWinMessage(guessesUsed);
+        } else {
+            this.message = "You lost! The word was: " + word;
+        }
     }
-
-    public String getWordToGuess() {
-        return wordToGuess;
+    private String generateWinMessage(int guesses) {
+        switch (guesses) {
+            case 1:
+                return "First try! The word was: " + word;
+            case 6:
+                return "That was close! The word was: " + word;
+            default:
+                return "You won! The word was: " + word;
+        }
+    }
+    public String getWord() {
+        return word;
     }
     public boolean isWon() {
         return won;
@@ -25,5 +40,8 @@ public class EndOutputData {
     }
     public int getMaxGuesses() {
         return maxGuesses;
+    }
+    public String getMessage() {
+        return message;
     }
 }
