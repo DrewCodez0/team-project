fd4ac79f6d147bdc73208dde860434b4bdc6c045
yub0wang1 <yubohenry.wang@mail.utoronto.ca>
2025-11-22 15:52:09 -0500

    Updated EndInputBoundary.java
    Updated EndInputBoundary.java, added prepareStartView to switch back to start view when the game ends

--- src/main/java/use_case/end/EndInputBoundary.java
+++ src/main/java/use_case/end/EndInputBoundary.java
@@ -1,15 +1,17 @@
 package use_case.end;
 
-import interface_adapter.game.GameState;
-
 /**
  * The End Use Case
  */
 public interface EndInputBoundary {
-
     /**
      * Execute the End Use Case
      * @param endInputData the input data for this use case
      */
-    void execute(GameState endInputData);
+    void execute(EndInputData endInputData);
+
+    void prepareStartView();
+
+    void prepareNewGame(EndInputData endInputData);
+
 }
