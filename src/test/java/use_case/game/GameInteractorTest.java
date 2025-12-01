93aff2f34d7996dbe22969a9620656f990fe9539
Roman <roman_rossi@outlook.com>
2025-12-01 07:29:21 -0500

    started making tests

---
+++ src/test/java/use_case/game/GameInteractorTest.java
@@ -0,0 +1,14 @@
+package use_case.game;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GameInteractorTest {
+}
